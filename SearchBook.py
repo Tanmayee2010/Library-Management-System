a0dc89702c4118fb82555240063d657ece41ef6e
Tanmayee <128023014+Tanmayee2010@users.noreply.github.com>
2024-06-06 09:51:00 +0530

    Add files via upload

---
+++ SearchBook.py
@@ -0,0 +1,28 @@
+from tkinter import *
+from tkinter import ttk
+from tkinter import messagebox
+import mysql.connector
+from mysql.connector import Error
+
+
+class Search(Tk):
+    def __init__(self):
+        super().__init__()
+        f = StringVar()
+        g = StringVar()
+        self.title("Search Book")
+        self.maxsize(800, 500)
+        self.minsize(800, 500)
+        self.canvas = Canvas(width=800, height=500, bg='gray')
+        self.canvas.pack()
+        self.iconbitmap(r"C:\Users\tanmayee patil\Downloads\favicon.ico")
+        l1=Label(text="Search Library",bg='gray', font=("Courier new",20,'bold')).place(x=290,y=20)
+        l = Label(self, text="Search By", bg='gray', font=("Courier new", 15, 'bold')).place(x=60, y=96)
+        def insert(data):
+            self.listTree.delete(*self.listTree.get_children())
+            for row in data:
+                self.listTree.insert("", 'end', text=row[0], values=(row[1], row[2], row[3]))
+
+
+
+Search().mainloop()
